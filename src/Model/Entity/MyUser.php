82008b0b37b2ea7fafe226e483aa1c85dec732c9
Roger Campanera <git@roger.pro>
2018-01-08 16:29:23 +0000

    Remove example method from users docs

--- src/Model/Entity/MyUser.php
+++ src/Model/Entity/MyUser.php
@@ -8,28 +8,6 @@ use CakeDC\Users\Model\Entity\User;
  */
 class MyUser extends User
 {
-    /**
-     * Map CakeDC's User.active field to User.is_active when getting
-     *
-     * @return mixed The value of the mapped property.
-     */
-    protected function _getActive()
-    {
-        return $this->_properties['is_active'];
-    }
-
-    /**
-     * Map CakeDC's User.active field to User.is_active when setting
-     *
-     * @param mixed $value The value to set.
-     * @return static
-     */
-    protected function _setActive($value)
-    {
-        $this->set('is_active', $value);
-        return $value;
-    }
-    
     public function parentNode() {
         return null;
     }
